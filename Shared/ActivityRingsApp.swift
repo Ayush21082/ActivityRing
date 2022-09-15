a6f44d7250ea4d30db9960ee0b1c885e07157039
Ayush Singh <ayush.singh21082@gmail.com>
2022-09-16 00:24:49 +0530

    Initial Commit

---
+++ Shared/ActivityRingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityRingsApp.swift
+//  Shared
+//
+//  Created by Ayush Singh on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ActivityRingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
